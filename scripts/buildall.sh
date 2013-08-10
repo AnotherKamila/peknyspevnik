30e204a4d148c31346e39fcea7c6a8b4b820c696
Kamila Souckova <kamila@vesmir.sk>
2013-08-10 09:22:37 +0200

    edit build script

--- scripts/buildall.sh
+++ scripts/buildall.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+if [[ $1 == -w ]]; then
+	WATCH=1
+	shift
+fi
+
 DIR=.
 DEST="${1-.}"
 
 EVENTS=modify,close_write,moved_to,create
 
+mkdir -p "$DEST"
+cp -r $DIR/* "$DEST"
 for f in `find -name '*.coffee'`; do
 	/bin/bash $(dirname "$0")/build.sh "$f" "$DEST"
 done
+
+[[ -z $WATCH ]] && exit
 
 echo "Watching $DIR (destination: $DEST)" >&2
 inotifywait -mrq -e $EVENTS --format '%e %w%f' "$DIR" | while read e; do
